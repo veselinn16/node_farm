77de4a65ecaa7712eb25be9d4c817488819b3618
Veselin Tonev <veselinn16@yahoo.com>
2019-07-29 17:57:32 +0300

    Replace HTML templates with product data in overview page :sparkles:

--- index.js
+++ index.js
@@ -29,23 +29,70 @@ const url = require("url");
 
 /////////////////////////
 // SERVER
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(
+    /{%NOT_ORGANIC%}/g,
+    product.organic ? "organic" : "not-organic"
+  );
+
+  return output;
+};
+
+// JSON
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
+// Array of JS ojects
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, response) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
-    response.end("Overview page mane!");
+    // Overview page
+    response.writeHead(200, {
+      "Content-type": "text/html"
+    });
+
+    // loop over products and replace template code with real data
+    const cardsHTML = dataObj
+      .map(el => replaceTemplate(templateCard, el))
+      .join("");
+
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
+
+    response.end(output);
   } else if (pathName === "/product") {
+    // Product page
     response.end("Products mane");
   } else if (pathName === "/api") {
+    // API
     response.writeHead(200, {
       "Content-type": "application/json"
     });
     response.end(data);
   } else {
+    // 404 page
     response.writeHead(404, {
       "Content-type": "text/html"
     });
