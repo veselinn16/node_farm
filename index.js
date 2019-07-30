e003cf3affe7f9974157381e056eda9e8bbe08b5
Veselin Tonev <veselinn16@yahoo.com>
2019-07-30 10:22:56 +0300

    Import replaceTemplate function from new module :recycle:

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 /////////////////////////
 // FILES
@@ -41,23 +42,6 @@ const templateProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(
-    /{%NOT_ORGANIC%}/g,
-    product.organic ? "organic" : "not-organic"
-  );
-
-  return output;
-};
 
 // JSON
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
