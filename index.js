37e5d7d4495f39b057ec350a0468c5763d606d14
Veselin Tonev <veselinn16@yahoo.com>
2019-07-30 11:47:11 +0300

    Remove unnecessary comments :fire:

--- index.js
+++ index.js
@@ -4,31 +4,6 @@ const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 /////////////////////////
-// FILES
-
-// Synchronous version of readFile
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-
-// const textOut = `This is what we know about the avocade: ${textIn}\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File is written!");
-
-// Asynchronous version of readFile
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
-//         console.log("File is written!");
-//       });
-//     });
-//   });
-// });
-// console.log("Will read file!");
-
-/////////////////////////
 // SERVER
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
